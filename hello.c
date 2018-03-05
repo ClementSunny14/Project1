5981d4a9ee17ffe05b90784cdda7a07267bc680e
ClementSunny14 <clementsunny140@gmail.com>
2018-03-05 11:11:31 +0530

    Hello

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{	
+	int i,k;
+	printf("You are entering the program\n");
+	scanf("%d",&i);
+	for(k=o;k<i;k++)
+	{
+	
+		printf("*"*i,"\n");
+	}
+	printf("You are leaving.Thankyou\n");
+
+}
